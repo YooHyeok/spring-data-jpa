b89bbeb9b3af53668aaa8fbfb7bcdf923c2d2f00
YooHyeok <yjk7454@naver.com>
2023-06-25 01:55:42 +0900

    Member 순수JPA 리포지토리 기능 추가

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,20 +5,45 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
     @PersistenceContext
     private EntityManager em;
 
-    /** 엔티티 저장 */
+    /** 회원 단건 저장 */
     public Member save(Member member) {
         em.persist(member);
         return member;
     }
 
-    /** 엔티티 조회 */
+    /** 회원 단건 조회 */
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    /** 회원 전체 조회 */
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    /** 회원 단건 조회(Optional) */
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    /** 회원 count 조회 */
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    /** 회원 삭제 */
+    public void delete(Member member) {
+        em.remove(member);
     }
 }
