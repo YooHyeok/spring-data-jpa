abfe378086c35ca51099eccd0dd2c7c44fa5d0df
YooHyeok <yjk7454@naver.com>
2023-06-24 12:31:46 +0900

    Member 엔티티, MemberJpaRepository추가 및 JPA기반 JUnit테스트

---
+++ src/main/java/study/datajpa/MemberJpaRepository.java
@@ -0,0 +1,23 @@
+package study.datajpa;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberJpaRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    /** 엔티티 저장 */
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    /** 엔티티 조회 */
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/study/datajpa/MemberJpaRepositoryTest.java
@@ -0,0 +1,30 @@
+package study.datajpa;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
+    }
+
+}
