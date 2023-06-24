51537776cc123195c4bc6ccd4de47d2067c1ffe6
YooHyeok <yjk7454@naver.com>
2023-06-24 12:36:56 +0900

    스프링 데이터 JPA 리포지토리 추가 및 테스트

---
+++ src/test/java/study/datajpa/MemberRepositoryTest.java
@@ -0,0 +1,29 @@
+package study.datajpa;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+        Member findById = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findById.getId()).isEqualTo(savedMember.getId());
+        assertThat(findById.getUsername()).isEqualTo(savedMember.getUsername());
+        assertThat(findById).isEqualTo(savedMember); //JPA 엔터티 동일성 보장 (영속성 1차캐시 Proxy객체)
+    }
+}
